1b32b81ae3d81c85d7c0923f16083660ded113a6
tiagobondoso <77240470+tiagobondoso@users.noreply.github.com>
2025-07-09 14:09:41 +0100

    Update NativeSettings.m

--- src/ios/NativeSettings.m
+++ src/ios/NativeSettings.m
@@ -9,7 +9,9 @@
     NSURL *settingsUrl = [NSURL URLWithString:UIApplicationOpenSettingsURLString];
 
     if ([[UIApplication sharedApplication] canOpenURL:settingsUrl]) {
-        [[UIApplication sharedApplication] openURL:settingsUrl];
+        [[UIApplication sharedApplication] openURL:settingsUrl options:@{} completionHandler:^(BOOL success) {
+            NSLog(@"Opened settings: %d", success);
+        }];
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Opened App Settings"];
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Cannot open settings"];
